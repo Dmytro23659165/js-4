0701e03bc136118a43f4855cc388923ff1f5720f
Dmytro23659165 <130238497+Dmytro23659165@users.noreply.github.com>
2024-08-03 19:38:50 +0300

    js-4\

---
+++ index.js
@@ -0,0 +1,50 @@
+// ДЗ L-4;
+let textFirst = "якщо ні lorem ipsum dolor amet";
+let textSecond = " якщо так tema rolod muspi meroL";
+let textert = textFirst + textSecond.includes(" якщо так");
+console.log(textert);
+
+
+let chislo = [10];
+let chisloTwo = [9];
+let two = chislo.includes(10);
+let three = chisloTwo.includes(10);
+console.log(two);
+console.log(three);
+
+
+let tox = "javaScript is the best";
+let toxer = "js is the best";
+let tor = tox.includes("javaScript");
+let torex = toxer.includes("javaScript");
+alert(tox);
+alert(toxer);
+console.log(tor);
+console.log(torex);
+
+
+let prom = prompt("xz");
+console.log(prom);
+let dorivnue = prom.includes(10);
+let dorivnu = prom.includes(11);
+let dorivn = prom.includes(12);
+let doriv = prom.includes(13);
+let dori = prom.includes(14);
+let dorivne = prom.includes(15);
+let dorive = prom.includes(16);
+let dorivv = prom.includes(17);
+let dorim = prom.includes(18);
+let dorimne = prom.includes(19);
+let dor = prom.includes(20);
+console.log(dorivnue);
+console.log(dorivnu);
+console.log(dorivn);
+console.log(doriv);
+console.log(dori);
+console.log(dorivne);
+console.log(dorive);
+console.log(dorivv);
+console.log(dorim);
+console.log(dorimne);
+console.log(dor);
+// ДЗ L-4 end;
